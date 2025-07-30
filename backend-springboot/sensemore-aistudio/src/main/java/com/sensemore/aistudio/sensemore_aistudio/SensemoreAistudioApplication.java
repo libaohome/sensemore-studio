1ef9a481205d2c095366058780df95743523ba52
bao.li001 <bao.li001@ke.com>
2025-07-30 10:10:39 +0800

    初始化springboot项目代码

---
+++ backend-springboot/sensemore-aistudio/src/main/java/com/sensemore/aistudio/sensemore_aistudio/SensemoreAistudioApplication.java
@@ -0,0 +1,13 @@
+package com.sensemore.aistudio.sensemore_aistudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensemoreAistudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensemoreAistudioApplication.class, args);
+	}
+
+}
---
+++ backend-springboot/sensemore-aistudio/src/test/java/com/sensemore/aistudio/sensemore_aistudio/SensemoreAistudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sensemore.aistudio.sensemore_aistudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensemoreAistudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
