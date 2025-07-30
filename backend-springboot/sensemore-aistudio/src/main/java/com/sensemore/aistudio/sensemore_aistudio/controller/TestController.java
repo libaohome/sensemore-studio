0673ca946c10c27fdbeff2370b33ae964bc7caaa
libaohome <libaohome@163.com>
2025-07-30 03:29:21 +0000

    helloworld测试代码

---
+++ backend-springboot/sensemore-aistudio/src/main/java/com/sensemore/aistudio/sensemore_aistudio/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.sensemore.aistudio.sensemore_aistudio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
+
