071e38cfcc92ab8ee6650d944d9d2098c567cb2c
bao.li001 <bao.li001@ke.com>
2025-11-11 13:53:44 +0800

    忽略spring ai自动配置

--- backend-springboot/sensemore-aistudio/src/main/java/com/sensemore/aistudio/SensemoreAistudioApplication.java
+++ backend-springboot/sensemore-aistudio/src/main/java/com/sensemore/aistudio/SensemoreAistudioApplication.java
@@ -3,8 +3,15 @@ package com.sensemore.aistudio;
 import com.sensemore.aistudio.factory.ChatClientFactory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiAudioSpeechAutoConfiguration;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiAudioTranscriptionAutoConfiguration;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiChatAutoConfiguration;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiEmbeddingAutoConfiguration;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiImageAutoConfiguration;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiModerationAutoConfiguration;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -13,6 +20,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @SpringBootApplication
 @MapperScan("com.sensemore.aistudio.mapper")
+@EnableAutoConfiguration(exclude = {
+		OpenAiAudioSpeechAutoConfiguration.class,
+		OpenAiAudioTranscriptionAutoConfiguration.class,
+		OpenAiEmbeddingAutoConfiguration.class,
+		OpenAiImageAutoConfiguration.class,
+		OpenAiChatAutoConfiguration.class,
+		OpenAiModerationAutoConfiguration.class
+})
 public class SensemoreAistudioApplication {
 
 	public static void main(String[] args) {
